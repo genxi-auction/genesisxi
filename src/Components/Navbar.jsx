ee59f78e2d9556c7860132c76c1104024340f6e7
EliteGoodDev <michaeljohnares.1026@gmail.com>
2025-02-26 09:12:17 +0100

    remove next auction content

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -10,27 +10,21 @@ const Navbar = () => {
   };
   return (
     <div className="bg-[#141a19]">
-      <header className="flex justify-between items-center px-6 py-4">
-        <h1 className="text-green-500 text-lg font-bold">Auction Live Now</h1>
-        <div className="text-center">
-          <p className="text-sm text-white">Next Auction</p>
-          <p className="text-lg font-bold text-white ">OD:OOH:OM:OS</p>
-        </div>
+      <header className="flex items-center justify-between px-6 py-4">
+        <h1 className="text-lg font-bold text-green-500">Auction Live Now</h1>
         <nav className="flex space-x-3 uppercase">
-          <button className="text-white  px-4 py-2 rounded">HOME</button>
+          <button className="px-4 py-2 text-white rounded">HOME</button>
           {isConnected ? (
             <button
-              className="text-white  px-4 py-2 rounded"
-                onClick={handleOpen}
-            >
+              className="px-4 py-2 text-white rounded"
+              onClick={handleOpen}>
               disconnect
             </button>
           ) : (
             <button
               type="button"
-              className="text-white  px-4 py-2 rounded"
-                onClick={handleOpen}
-            >
+              className="px-4 py-2 text-white rounded"
+              onClick={handleOpen}>
               CONNECT
             </button>
           )}
